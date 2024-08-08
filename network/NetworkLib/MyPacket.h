5e8538a0d73cd2747b110bcc82b666d72b4e5f88
Samana0104 <b_hb0104@naver.com>
2024-08-08 22:02:45 +0900

    Added a packet

--- network/NetworkLib/MyPacket.h
+++ network/NetworkLib/MyPacket.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "pch.h"
 
 using uint_8 = unsigned char;
 
@@ -6,10 +7,14 @@ enum class PacketHeader : uint_8
 {
 	SERV_CONNECT_SEND,
 	SERV_CONNECT_REQUEST,	
-
+	CHAT_MSG_SEND,
 };
+
+const int PACKET_HEADER_SIZE = sizeof(uint_8);
+const int PACKET_MSG_SIZE = 1024;
 
 struct BytePacket
 {
 	PacketHeader m_header;
+	BYTE		 m_msg[PACKET_MSG_SIZE];
 };
--- network/NetworkLib/pch.h
+++ network/NetworkLib/pch.h
@@ -1,7 +1,22 @@
+#pragma once
 #include <Windows.h>
 #include <string>
 #include <string_view>
 #include <thread>
+#include <iostream>
+#include <memory>
 #include <WinSock2.h>
-#include <vector>
+#include <WS2tcpip.h>
+#include <atlconv.h> // A2W
+
+// -----------------------------------
+//			Containers
+// -----------------------------------
 #include <map>
+#include <vector>
+#include <list>
+#include <string>
+#include <map>
+
+#pragma comment(lib,"ws2_32.lib")
+
