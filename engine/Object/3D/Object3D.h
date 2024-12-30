8ddae272d34e562f2b26210ef903bbd1ac910d95
samana <b_hb0104@naver.com>
2024-12-30 21:55:32 +0900

    Added a camera

--- engine/Object/3D/Object3D.h
+++ engine/Object/3D/Object3D.h
@@ -32,6 +32,9 @@ namespace HBSoft
         ~Object3D() = default;
 
     public:
+        Transform3D m_transform;
+
+    public:
         void SetLight(std::shared_ptr<DirectionalLight> light);
 
         virtual void ProcessCollision(std::shared_ptr<Object3D> obj);
