d3b43cba061db48f24db2a2e105611d228371301
jcb <kgcaN@kgcaschool.com>
2024-12-31 18:43:57 +0900

    save

--- engine/Ai/GardenerFSM.cpp
+++ engine/Ai/GardenerFSM.cpp
@@ -8,7 +8,6 @@ void HBSoft::GardenerFSM::onEvent(Event event)
     case State::Idle:
         if (event == Event::GooseClose)
         {
-
             // 여기다 행동
             currentState = State::Pushing;
         }
